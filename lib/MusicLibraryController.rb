bc1e0bb265b90fc8ee79e71b4fdbce8ca2caaf1b
Marci Clark <marcig612@gmail.com>
2019-12-02 20:45:01 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -23,18 +23,18 @@ class MusicLibraryController
     if input != exit
 
     case user_input
-    when "list songs"
-      self.list_songs
-    when "list artists"
-      self.list_artists
-    when "list genres"
-      self.list_genres
-    when "list artist"
-      self.list_songs_by_artist
-    when "list genre"
-      self.list_songs_by_genre
-    when "play song"
-      self.play_song
+      when "list songs"
+        self.list_songs
+      when "list artists"
+        self.list_artists
+      when "list genres"
+        self.list_genres
+      when "list artist"
+        self.list_songs_by_artist
+      when "list genre"
+        self.list_songs_by_genre
+      when "play song"
+        self.play_song
     end
   end
 
